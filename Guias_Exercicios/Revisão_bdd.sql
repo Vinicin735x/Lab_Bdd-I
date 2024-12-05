7cf21490bf25ed1c65409f0a7d84ef53ca2c5245
Vinicin735x <vini08.mantovani@gmail.com>
2024-12-05 12:17:33 -0300

    Atualização do banco

--- Guias_Exercicios/Revisão_bdd.sql
+++ Guias_Exercicios/Revisão_bdd.sql
@@ -40,3 +40,51 @@ select * from TbPessoa;
 -- delete from TbPessoa where idPessoa= 5; /* não deu mensagem de erro, mas é devido ao fato que não existe idPessoa= 5, por isso vamos mudar de comando*/
 delete from TbPessoa where idPessoa= 4;
 select * from TbPessoa
+
+-- passo 10 acrescentar a tabela tbcidade
+create table TbCidade(
+	idcidade int auto_increment,
+    nomecid varchar(255),
+    estado char(2)
+)engine=InnoDB;
+
+alter table TbCidade modify column idcidade int auto_increment primary key; /* eu tinha colocado a tabela para na hora de passar os valores, colocar o id também, após essa mudança vai automático*/
+
+-- passo 11 Inserir 3 cidades
+insert into Tbcidade (nomecid, estado) values
+("Varginha", "Mg"),
+("Lavras", "Mg"),
+("São Paulo", "Sp");
+
+-- passo 12 Uma pessoa reside em uma cidade e em uma cidade residem várias pessoas. Estabelecer tal relacionamento entre tbpessoa e tbcidade. (1:N)
+alter table tbpessoa add column idcidmora int;
+alter table TbPessoa add foreign key(idcidmora) references Tbcidade(idcidade);
+
+-- passo 13 Inserir uma pessoa que se relacione com a cidade de código cujo valor seja 200
+ALTER TABLE TbPessoa ADD COLUMN idCidade INT, ADD CONSTRAINT fk_pessoa_cidade FOREIGN KEY (idCidade) REFERENCES TbCidade(idCidade);
+-- Cria uma restrição de chave estrangeira chamada fk_pessoa_cidade na coluna idCidade da tabela TbPessoa. Essa restrição indica que os valores da coluna idCidade em TbPessoa devem existir na coluna idCidade da tabela TbCidade.
+
+insert into TbPessoa (nome, CPF, idCidade) values
+("Carlos", 111777, 200);
+-- esse comando da errado, pois não existe uma cidade com o id 200
+
+-- passo 14 Relacionar as pessoas já cadastradas com cidades existentes
+update TbPessoa set idcidade= 2 where idPessoa= 1;
+update TbPessoa set idcidade= 1 where idPessoa= 3;
+update TbPessoa set idcidade= 3 where idPessoa= 2;
+select * from TbPessoa;
+
+-- passo 15 Excluir a cidade chamada “usar um nome de cidade que você cadastrou” 
+delete from TbCidade where idCidade= 1; /* erro */
+alter table TbPessoa drop foreign key fk_pessoa_cidade;
+alter table TbPessoa drop foreign key tbpessoa_ibfk_1;
+alter table TbPessoa add foreign key(idcidmora) references tbcidade(idcidade) on delete cascade on update cascade;
+update tbPessoa set idcidade= 2 where idPessoa= 3;
+delete from TbCidade where idCidade= 2;
+select * from Tbcidade;
+select * from TbPessoa;
+
+-- passo extra, vou fazer testes
+insert into Tbcidade (nomecid, estado) values
+("Varginha", "Mg"),
+("Lavras", "Mg")
